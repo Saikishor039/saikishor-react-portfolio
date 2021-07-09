ac739999d0d378b3dafd19bb80c12791fcb8b565
Saikishor039 <80215316+Saikishor039@users.noreply.github.com>
2021-07-09 00:18:58 -0400

    Add files via upload

---
+++ build/precache-manifest.8ed712c2f0b24ee8d54b71856bcc85f2.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0273e8fac2ac1a9ef139fbfe4070fa4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84b837743f9936b24230",
+    "url": "/static/css/2.488f0ad5.chunk.css"
+  },
+  {
+    "revision": "c65ed84bc5c9a1c27c34",
+    "url": "/static/css/main.fe8339e0.chunk.css"
+  },
+  {
+    "revision": "84b837743f9936b24230",
+    "url": "/static/js/2.8210c8d9.chunk.js"
+  },
+  {
+    "revision": "dab61cd72293cff32e597ebab79267a2",
+    "url": "/static/js/2.8210c8d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c65ed84bc5c9a1c27c34",
+    "url": "/static/js/main.b5a9c5a2.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "87ab9e20190df9ec4edfb0788e89f211",
+    "url": "/static/media/Hyderabadjourney.87ab9e20.png"
+  },
+  {
+    "revision": "b2bff4cc7d709bdec9ef93ef2fbc1e5c",
+    "url": "/static/media/Web devloper_Resume.b2bff4cc.docx"
+  },
+  {
+    "revision": "90612ea9b098c284414a7b1bcfad18bb",
+    "url": "/static/media/aacismaharjan-loading-logo.90612ea9.svg"
+  },
+  {
+    "revision": "fc6619de6848465a5656350eb2701c29",
+    "url": "/static/media/amazon.fc6619de.png"
+  },
+  {
+    "revision": "b65312c88d2bcf294050f78fbbf78e40",
+    "url": "/static/media/app-landingpage.b65312c8.png"
+  },
+  {
+    "revision": "432d7724e06d6dd3bf9b4d00da47a0d7",
+    "url": "/static/media/coolors.432d7724.jpg"
+  },
+  {
+    "revision": "b738210fe19e69bb4620e9543983c2df",
+    "url": "/static/media/covid-19.b738210f.png"
+  },
+  {
+    "revision": "459985af8d7267d950404741484014a5",
+    "url": "/static/media/getwatch.459985af.jpg"
+  },
+  {
+    "revision": "07efa94cda3cd5620d3cd76655ffe5e2",
+    "url": "/static/media/github_user_stats.07efa94c.jpg"
+  },
+  {
+    "revision": "49e03ef596eff4ba8a69ef8d4fc90ace",
+    "url": "/static/media/itlabs.49e03ef5.png"
+  },
+  {
+    "revision": "0bbcde6d16229565d8fc305966aa8a66",
+    "url": "/static/media/landing_page_template.0bbcde6d.jpg"
+  },
+  {
+    "revision": "156371b9d8a2b502eb88e98ce978db12",
+    "url": "/static/media/mern-twitter-clone.156371b9.png"
+  },
+  {
+    "revision": "288825a4ec217476ae47967453c3687f",
+    "url": "/static/media/quizbee.288825a4.jpg"
+  },
+  {
+    "revision": "f27da5af0220c4c08aa50a5ecdf61b49",
+    "url": "/static/media/rms.f27da5af.jpg"
+  }
+]);
